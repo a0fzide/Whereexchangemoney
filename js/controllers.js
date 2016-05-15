645ba49cd30119cafecacc0264cd1aa250df64b5
a0fzide <a0fzide@gmail.com>
2016-05-15 08:45:17 +0700

    add swap

--- js/controllers.js
+++ js/controllers.js
@@ -2,5 +2,5 @@ app.controller('mapCtrl', function($scope, CurrenciesFrom, CurrenciesTo) {
     $scope.exchangeFrom = CurrenciesFrom.all();
     $scope.exchangeTo = CurrenciesTo.all();
     // console.log($scope.exchangeFrom);
-    $scope.items = [1, 2, 3, 4, 5];
+    $scope.buying = true;
 });
