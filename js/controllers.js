c5f97d2b51631272a2121f391d5db1dde7e8d35f
a0fzide <a0fzide@gmail.com>
2016-05-15 10:29:23 +0700

    initial data select

--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,15 @@
-app.controller('mapCtrl', function($scope, CurrenciesFrom, CurrenciesTo, Shops, ExchangeRates) {
+app.controller('mapCtrl', function($scope, CurrenciesFrom, CurrenciesTo, Shops, ExchangeRates, $timeout) {
     $scope.buying = true;
     $scope.exchangeFrom = CurrenciesFrom.all();
     $scope.exchangeTo = CurrenciesTo.all();
     // console.log($scope.exchangeFrom);
     
     $scope.shops = Shops.all();
+    console.log($scope.selectedFrom);
     $scope.exchangeRates = ExchangeRates.all();
+    $timeout(function() {
+        $scope.selectedFrom = $scope.exchangeFrom[0];
+        $scope.selectedTo = $scope.exchangeTo[0];
+        // console.log($scope.selectedFrom);
+    }, 500);
 });
